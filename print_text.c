512a473be004518df11dc37a53858dfe4f14f110
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-04 09:41:38 -0300

    Learning to print a text

---
+++ print_text.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // To output values or print text in C, you can use the printf() function:
+    printf("Hello World!");
+
+    // You can use as many printf() functions as you want. However, note that it does not insert a new line at the end of the output.
+    // making it difficult to understand
+    printf("I am learning C.");
+
+    return 0;
+}
